f763b75fb3e700cdc05de266b56f062047a1ccb4
GitHub Action <action@github.com>
2020-06-10 13:48:53 +0530

    Charts

--- tests.py
+++ tests.py
@@ -1,1 +1,30 @@
+#### import testing modules
 import pytest
+import unittest
+#### import modules to be tested
+from data import Data
+from current_mood import Current
+from user import User
+from headline import Headline
+
+#### test database
+def test_db():
+    pass
+
+##### tested data from data.py
+def test_data():
+    pass
+
+#### test user login 
+def test_user():
+    pass
+
+#### test current mood from current_mood.py
+def test_current_mood():
+    pass
+
+#### test headlines from news.py
+def test_headline():
+    pass
+
+#### test client and get pages
